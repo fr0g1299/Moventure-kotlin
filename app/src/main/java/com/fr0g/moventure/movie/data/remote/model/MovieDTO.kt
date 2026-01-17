381d74e9a943c77ba760568515b7caa7ca7bc453
fr0g1299 <dodoloka1299@gmail.com>
2026-01-17 17:21:15 +0100

    Add movie data models, DTOs, and genre utility constants

---
+++ app/src/main/java/com/fr0g/moventure/movie/data/remote/model/MovieDTO.kt
@@ -0,0 +1,17 @@
+package com.fr0g.moventure.movie.data.remote.model
+
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class MovieDTO(
+    @SerialName("page")
+    val page: Int? = null,
+    @SerialName("results")
+    val results: List<Result?>? = null,
+    @SerialName("total_pages")
+    val totalPages: Int? = null,
+    @SerialName("total_results")
+    val totalResults: Int? = null
+)
