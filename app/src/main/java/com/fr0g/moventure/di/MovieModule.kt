ab97707b06d477caaa7ae0fda70395ed586c27c3
fr0g1299 <dodoloka1299@gmail.com>
2026-01-17 17:26:33 +0100

    Add generic response utility and implement movie dependency injection module

---
+++ app/src/main/java/com/fr0g/moventure/di/MovieModule.kt
@@ -0,0 +1,50 @@
+package com.fr0g.moventure.di
+
+import com.fr0g.moventure.common.data.ApiMapper
+import com.fr0g.moventure.movie.data.implementation.MovieApiMapper
+import com.fr0g.moventure.movie.data.remote.api.MovieApiService
+import com.fr0g.moventure.movie.data.remote.model.MovieDTO
+import com.fr0g.moventure.movie.domain.models.Movie
+import com.fr0g.moventure.movie.domain.repository.MovieRepository
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import com.fr0g.moventure.movie.data.implementation.MovieRepository as MRI
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object MovieModule {
+    private val json = Json {
+        coerceInputValues = true
+        ignoreUnknownKeys = true
+    }
+
+    @Provides
+    @Singleton
+    fun provideMovieRepository(
+        movieApiService: MovieApiService,
+        mapper:ApiMapper<List<Movie>, MovieDTO>
+    ): MovieRepository = MRI(
+        movieApiService, mapper
+    )
+
+    @Provides
+    @Singleton
+    fun provideMovieMapper(): ApiMapper<List<Movie>, MovieDTO> = MovieApiMapper()
+
+    @Provides
+    @Singleton
+    fun provideMovieApiService():MovieApiService{
+        val contentType = "application/json".toMediaType()
+        return Retrofit.Builder().baseUrl("https://api.themoviedb.org/3/")
+            .addConverterFactory(json.asConverterFactory(contentType))
+            .build()
+            .create(MovieApiService::class.java)
+    }
+}
