d04b872c345ca3ea9f020b297c318ae7e68f62cf
fr0g1299 <dodoloka1299@gmail.com>
2026-01-17 17:29:15 +0100

    Add application class and annotate MainActivity for Hilt dependency injection

---
+++ app/src/main/java/com/fr0g/MovieApplication.kt
@@ -0,0 +1,9 @@
+package com.fr0g
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MovieApplication:Application() {
+
+}
--- app/src/main/java/com/fr0g/moventure/MainActivity.kt
+++ app/src/main/java/com/fr0g/moventure/MainActivity.kt
@@ -11,8 +11,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.fr0g.moventure.ui.theme.MoventureTheme
+import com.fr0g.moventure.ui.MoventureTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
