16952efdc29481964fb67f1868941ef952113847
fr0g1299 <dodoloka1299@gmail.com>
2026-01-17 17:28:15 +0100

    Add HomeViewModel, HomeState, and Flow extension for handling movie repository responses

---
+++ app/src/main/java/com/fr0g/moventure/ui/home/HomeViewModel.kt
@@ -0,0 +1,73 @@
+package com.fr0g.moventure.ui.home
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.fr0g.moventure.movie.data.implementation.MovieRepository
+import com.fr0g.moventure.movie.domain.models.Movie
+import com.fr0g.moventure.utils.collectAndHandle
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+    private val repository: MovieRepository,
+):ViewModel() {
+    private val _homeState = MutableStateFlow(HomeState())
+    val homeState = _homeState.asStateFlow()
+
+    init {
+        fetchDiscoverMovie()
+    }
+    init {
+        fetchTrendingMovie()
+    }
+
+    private fun fetchDiscoverMovie() = viewModelScope.launch {
+        repository.fetchDiscoverMovie().collectAndHandle(
+            onError = {error ->
+                _homeState.update {
+                    it.copy(isLoading = false, error = error?.message)
+                }
+            },
+            onLoading = {
+                _homeState.update {
+                    it.copy(isLoading = true, error = null)
+                }
+            }
+        ) { movie ->
+            _homeState.update {
+                it.copy(isLoading = false, error = null, discoverMovies = movie)
+            }
+        }
+    }
+
+    private fun fetchTrendingMovie() = viewModelScope.launch {
+        repository.fetchTrendingMovie().collectAndHandle(
+            onError = {error ->
+                _homeState.update {
+                    it.copy(isLoading = false, error = error?.message)
+                }
+            },
+            onLoading = {
+                _homeState.update {
+                    it.copy(isLoading = true, error = null)
+                }
+            }
+        ) { movie ->
+            _homeState.update {
+                it.copy(isLoading = false, error = null, trendingMovies = movie)
+            }
+        }
+    }
+}
+
+data class HomeState(
+    val discoverMovies:List<Movie> = emptyList(),
+    val trendingMovies:List<Movie> = emptyList(),
+    val error: String? = null,
+    val isLoading: Boolean = false
+)
---
+++ app/src/main/java/com/fr0g/moventure/utils/Extension.kt
@@ -0,0 +1,28 @@
+package com.fr0g.moventure.utils
+
+import android.util.Log
+import kotlinx.coroutines.flow.Flow
+
+suspend fun <T> Flow<Response<T>>.collectAndHandle(
+    onError: (Throwable?) -> Unit = {
+        Log.e("collectAndHandle", "collectAndHandle: error")
+    },
+    onLoading: () -> Unit = {},
+    stateReducer: (T) -> Unit,
+) {
+    collect { response ->
+        when (response) {
+            is Response.Error -> {
+                onError(response.error)
+            }
+
+            is Response.Success -> {
+                stateReducer(response.data)
+            }
+
+            is Response.Loading -> {
+                onLoading()
+            }
+        }
+    }
+}
